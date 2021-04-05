a510317705869c3e165ac990f41071984c303d73
JenChieh <jcs090218@gmail.com>
2021-04-05 23:40:28 +0800

    Add core

---
+++ eglot-languagetool.el
@@ -0,0 +1,38 @@
+;;; eglot-languagetool.el ---   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Shen, Jen-Chieh
+;; Created date 2021-04-05 23:38:07
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description:
+;; Keyword:
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs090218/eglot-languagetool
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;;
+;;
+
+;;; Code:
+
+
+
+(provide 'eglot-languagetool)
+;;; eglot-languagetool.el ends here
